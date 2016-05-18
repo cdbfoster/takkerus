fd2e487f443f1b2cdac7d0369275397604c301fe
Chris Foster <cdbfoster@gmail.com>
2016-05-17 20:44:29 -0400

    Discard the SDL stuff for now; add in a small command line interface.

--- src/main.rs
+++ src/main.rs
@@ -19,73 +19,27 @@
 
 #[macro_use]
 extern crate lazy_static;
-extern crate sdl2;
-extern crate sdl2_gfx;
-extern crate time;
 
-use sdl2::event::Event;
-
-//mod render;
 mod tak;
-mod timer;
 
-//use render::Renderable;
-use timer::Timer;
+use tak::Player;
 
 fn main() {
     let mut state = tak::State::new(5);
 
-    state = state.execute_ply(&tak::Ply::from_ptn("Ca1", tak::Color::White).unwrap()).ok().unwrap();
-    state = state.execute_ply(&tak::Ply::from_ptn("Sb1", tak::Color::Black).unwrap()).ok().unwrap();
-    state = state.execute_ply(&tak::Ply::from_ptn("a1>", tak::Color::White).unwrap()).ok().unwrap();
-    state = state.execute_ply(&tak::Ply::from_ptn("2b1+11", tak::Color::White).unwrap()).ok().unwrap();
-    println!("{:?}", state);
+    let mut p1 = tak::cli_player::CliPlayer::new(tak::Color::White);
+    let mut p2 = tak::cli_player::CliPlayer::new(tak::Color::Black);
 
-    let sdl_context = sdl2::init().unwrap();
-    let mut sdl_event_pump = sdl_context.event_pump().unwrap();
-    //let mut render_context = render::initialize(&sdl_context, (1366, 768), false);
-
-    let mut input_timer = Timer::new(1.0 / 30.0);
-    let mut render_timer = Timer::new(1.0 / 45.0);
-
-    let mut old_time = time::precise_time_ns();
-    'main: loop {
-        let elapsed_time = (time::precise_time_ns() - old_time) as i64;
-        input_timer.elapse(elapsed_time);
-        render_timer.elapse(elapsed_time);
-        old_time += elapsed_time as u64;
-
-        if input_timer.sprung() {
-            for event in sdl_event_pump.poll_iter() {
-                match event {
-                    Event::Quit {..} => break 'main,
-
-                    _ => (),
-                }
-            }
-
-            input_timer.reset_with_overflow();
-        }
-
-        if render_timer.sprung() {
-            //render::clear(&mut render_context);
-
-            //board.render(&mut render_context);
-
-            //render::present(&mut render_context);
-
-            render_timer.reset_with_overflow();
-        }
-
-        let sleep = {
-            let remainders = vec![
-                input_timer.remainder_ns(),
-                render_timer.remainder_ns(),
-            ];
-
-            let min = *remainders.iter().min().unwrap() - (time::precise_time_ns() - old_time) as i64;
-            if min > 0 { min as u32 } else { 0 }
+    loop {
+        let p = if state.ply_count % 2 == 0 {
+            &mut p1
+        } else {
+            &mut p2
         };
-        std::thread::sleep(std::time::Duration::new(0, sleep));
+
+        println!("{:?}", state);
+
+        let ply = p.get_move(&state);
+        state = state.execute_ply(&ply).unwrap();
     }
 }
