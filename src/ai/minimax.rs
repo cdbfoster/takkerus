c61112143d5279eb7d3078a4931e0993f1421efa
Chris Foster <cdbfoster@gmail.com>
2016-06-22 17:50:59 -0400

    Added tests to the minimax module.

--- src/ai/minimax.rs
+++ src/ai/minimax.rs
@@ -160,3 +160,150 @@ fn minimax(state: &State, mut move_set: Vec<Ply>, depth: u8, mut alpha: Eval, be
 
     (move_set, best_eval)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{cmp, f32};
+    use time;
+
+    use tak::*;
+    use super::{MIN_EVAL, MAX_EVAL, minimax};
+
+    #[test]
+    #[ignore]
+    fn test_minimax() {
+        let mut state = State::new(5);
+
+        let depth = 5;
+
+        let mut p1_min_time = f32::MAX;
+        let mut p1_max_time = 0.0;
+        let mut p1_total_time = 0.0;
+
+        let mut p2_min_time = f32::MAX;
+        let mut p2_max_time = 0.0;
+        let mut p2_total_time = 0.0;
+
+        let mut ply_count = 0;
+
+        loop {
+            let old_time = time::precise_time_ns();
+
+            let (plies, eval) = minimax(&state, Vec::new(), depth, MIN_EVAL, MAX_EVAL);
+
+            let elapsed_time = (time::precise_time_ns() - old_time) as f32 / 1000000000.0;
+
+            if ply_count % 2 == 0 {
+                if elapsed_time < p1_min_time {
+                    p1_min_time = elapsed_time;
+                }
+
+                if elapsed_time > p1_max_time {
+                    p1_max_time = elapsed_time;
+                }
+
+                p1_total_time += elapsed_time;
+            } else {
+                if elapsed_time < p2_min_time {
+                    p2_min_time = elapsed_time;
+                }
+
+                if elapsed_time > p2_max_time {
+                    p2_max_time = elapsed_time;
+                }
+
+                p2_total_time += elapsed_time;
+            }
+
+            ply_count += 1;
+
+            match state.execute_ply(&plies[0]) {
+                Ok(next) => {
+                    state = next;
+
+                    match state.check_win() {
+                        Win::None => (),
+                        _ => break,
+                    }
+                },
+                Err(error) => panic!("Minimax returned an illegal move.\n--------------------------------------------------\n{}\n{:?}\nError: {}", state, plies[0], error),
+            }
+
+            if ply_count % 10 == 0 {
+                println!("--------------------------------------------------");
+                println!("{}", state);
+                println!("{:?}\n", state.analysis);
+            }
+
+            println!("{}: {:3} {:6} {:7.3} {:7.3} {:8.3}", ply_count,
+                if ply_count % 2 == 1 {
+                    "White"
+                } else {
+                    "Black"
+                },
+                eval,
+                elapsed_time,
+                if ply_count % 2 == 1 {
+                    p1_total_time / ((ply_count + 1) / 2) as f32
+                } else {
+                    p2_total_time / ((ply_count + 1) / 2) as f32
+                },
+                if ply_count % 2 == 1 {
+                    p1_total_time
+                } else {
+                    p2_total_time
+                },
+            );
+        }
+
+        match state.check_win() {
+            Win::Road(color) => match color {
+                Color::White => println!("White wins. (R-0)"),
+                Color::Black => println!("Black wins. (0-R)"),
+            },
+            Win::Flat(color) => match color {
+                Color::White => println!("White wins. (F-0)"),
+                Color::Black => println!("Black wins. (0-F)"),
+            },
+            Win::Draw => println!("Draw. (1/2-1/2)"),
+            _ => (),
+        }
+
+        println!("--------------------------------------------------");
+        println!("{}", state);
+        println!("{:?\n}", state.analysis);
+
+        println!("Plies: {}", ply_count);
+        println!("Minimum ply time: {:.3}", if p1_min_time < p2_min_time {
+            p1_min_time
+        } else {
+            p2_min_time
+        });
+        println!("Maximum ply time: {:.3}", if p1_max_time > p2_max_time {
+            p1_max_time
+        } else {
+            p2_max_time
+        });
+        println!("Average ply time: {:.3}", (p1_total_time + p2_total_time) / ply_count as f32);
+        println!("Game time: {:.3}", p1_total_time + p2_total_time);
+    }
+
+    #[test]
+    fn test_eval() {
+        let state = State::from_tps("[TPS \"112S,12S,x1,1,x1/2,2221C,22112C,x2/x1,22,2,12,x1/2,22,x1,12,x1/21,x2,21,x1 1 35\"]").unwrap();
+        println!("{}", state);
+        println!("{:?}\n", state.analysis);
+
+        let (plies, eval) = minimax(&state, Vec::new(), 5, MIN_EVAL, MAX_EVAL);
+
+        for (i, ply) in plies.iter().enumerate() {
+            println!("{}: {:?}", if (state.ply_count + i as u16) % 2 == 0 {
+                "W"
+            } else {
+                "B"
+            }, ply);
+        }
+
+        println!("{}", eval);
+    }
+}
