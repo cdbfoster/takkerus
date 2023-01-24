c88e0c5ac563825f0ec49105550758f13dd76699
Chris Foster <cdbfoster@gmail.com>
2023-01-24 17:20:07 -0500

    Implemented (most of) Topaz's eval.

--- analysis/src/evaluation/mod.rs
+++ analysis/src/evaluation/mod.rs
@@ -1,14 +1,15 @@
-use tak::{Color, Resolution, State};
+use tak::{Bitmap, Color, PieceType, Resolution, State};
 
 pub use self::types::Evaluation;
+pub(crate) use crate::util::placement_threat_map;
 
-use self::scoring::*;
+use self::types::EvalType;
 
-mod scoring;
 mod types;
 
-pub fn evaluate<const N: usize>(state: &State<N>, start_ply: u16) -> Evaluation {
+pub fn evaluate<const N: usize>(state: &State<N>, _start_ply: u16) -> Evaluation {
     use Color::*;
+    use PieceType::*;
 
     match state.resolution() {
         None => (),
@@ -33,38 +34,221 @@ pub fn evaluate<const N: usize>(state: &State<N>, start_ply: u16) -> Evaluation 
     let p1_road_pieces = road_pieces & m.p1_pieces;
     let p2_road_pieces = road_pieces & m.p2_pieces;
 
-    let blocking_pieces = m.standing_stones | m.capstones;
-    let p1_blocking_pieces = blocking_pieces & m.p1_pieces;
-    let p2_blocking_pieces = blocking_pieces & m.p1_pieces;
+    for x in 0..N {
+        for y in 0..N {
+            let stack = &state.board[x][y];
+            // Top piece bonus
+            if let Some(piece) = stack.last() {
+                let piece_type = match piece.piece_type() {
+                    Flatstone => WEIGHT.pieces[0],
+                    StandingStone => WEIGHT.pieces[1],
+                    Capstone => WEIGHT.pieces[2],
+                };
 
-    // Material
-    p1_eval += evaluate_material(m, m.p1_pieces);
-    p2_eval += evaluate_material(m, m.p2_pieces);
+                let location = if piece.piece_type() == Capstone && stack.len() == 1 {
+                    2 * WEIGHT.location[x][y]
+                } else {
+                    WEIGHT.location[x][y]
+                };
 
-    // Blocker locations
-    p1_eval += evaluate_blocker_locations(p1_blocking_pieces);
-    p2_eval += evaluate_blocker_locations(p2_blocking_pieces);
+                match piece.color() {
+                    White => p1_eval += piece_type + location,
+                    Black => p2_eval += piece_type + location,
+                }
+            }
 
-    // Road groups
-    p1_eval += evaluate_road_groups(p1_road_pieces);
-    p2_eval += evaluate_road_groups(p2_road_pieces);
+            // Friendlies and Captives
+            if stack.len() > 1 {
+                let top_piece = stack.last().unwrap();
+                let top_piece_type = top_piece.piece_type();
 
-    // Road slices
-    p1_eval += evaluate_road_slices(p1_road_pieces);
-    p2_eval += evaluate_road_slices(p2_road_pieces);
+                // Bonus for hard caps
+                if top_piece_type == Capstone
+                    && stack.get(stack.len() - 2).unwrap().color() == top_piece.color()
+                {
+                    match top_piece.color() {
+                        White => p1_eval += 30,
+                        Black => p2_eval += 30,
+                    }
+                }
 
-    // Captured flats
-    p1_eval += evaluate_captured_flats(m.p1_pieces, &m.p1_stacks, &m.p2_stacks);
-    p2_eval += evaluate_captured_flats(m.p2_pieces, &m.p2_stacks, &m.p1_stacks);
+                let mut safety = 0;
+                let mut mobility = 0;
 
-    // Placement threats
-    p1_eval += evaluate_placement_threats(p1_road_pieces, all_pieces & !p1_road_pieces);
-    p2_eval += evaluate_placement_threats(p2_road_pieces, all_pieces & !p2_road_pieces);
+                match top_piece_type {
+                    StandingStone => safety += 16,
+                    Capstone => {
+                        safety += 64;
+                        mobility += 1;
+                    }
+                    _ => (),
+                }
 
-    // Tempo offset
-    match state.to_move() {
-        White => p1_eval += tempo_offset::<N>(start_ply, state.ply_count),
-        Black => p2_eval += tempo_offset::<N>(start_ply, state.ply_count),
+                let neighbors = {
+                    let mut bit = Bitmap::<N>::default();
+                    bit.set(x, y);
+                    (bit.dilate() ^ bit)
+                        .bits()
+                        .map(|b| b.coordinates())
+                        .map(|(x, y)| &state.board[x][y])
+                };
+
+                for neighbor in neighbors {
+                    if let Some(neighbor_top) = neighbor.last() {
+                        if neighbor_top.color() == top_piece.color() {
+                            match neighbor_top.piece_type() {
+                                Flatstone => {
+                                    safety += 1;
+                                    mobility += 1;
+                                }
+                                StandingStone => {
+                                    if neighbor.len() < N {
+                                        safety += 4;
+                                    }
+                                }
+                                Capstone => {
+                                    if neighbor.len() < N {
+                                        safety += 32;
+                                    }
+                                    mobility += 1;
+                                }
+                            }
+                        } else {
+                            match neighbor_top.piece_type() {
+                                Flatstone => mobility += 2,
+                                StandingStone => {
+                                    if neighbor.len() < N {
+                                        safety -= 4;
+                                    }
+                                }
+                                Capstone => {
+                                    if neighbor.len() < N {
+                                        safety -= 32;
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        mobility += 2;
+                    }
+                }
+
+                let (player_stacks, opponent_stacks) = match state.to_move() {
+                    White => (&m.p1_stacks, &m.p2_stacks),
+                    Black => (&m.p2_stacks, &m.p1_stacks),
+                };
+
+                let (mut captives, mut friendlies) = (
+                    player_stacks[x][y].count_ones() as EvalType - 1,
+                    opponent_stacks[x][y].count_ones() as EvalType,
+                );
+
+                if mobility < 2 && top_piece_type != Flatstone {
+                    friendlies /= 2;
+                }
+
+                if safety < 0 {
+                    captives *= 2;
+                }
+
+                let (captive_mul, friendly_mul) = match top_piece.piece_type() {
+                    Flatstone => WEIGHT.piece_mul[0],
+                    StandingStone => WEIGHT.piece_mul[1],
+                    Capstone => WEIGHT.piece_mul[2],
+                };
+
+                let stack_score = captives * captive_mul + friendlies * friendly_mul;
+                match top_piece.color() {
+                    White => p1_eval += stack_score,
+                    Black => p2_eval += stack_score,
+                }
+            }
+        }
+    }
+
+    let p1_caps = m.capstones & m.p1_pieces;
+    if p1_caps != 0.into() {
+        let neighbors = p1_caps.dilate() & (m.flatstones | m.standing_stones);
+        if neighbors == 0.into() {
+            p2_eval += 30;
+        }
+    }
+
+    let p2_caps = m.capstones & m.p1_pieces;
+    if p2_caps != 0.into() {
+        let neighbors = p2_caps.dilate() & (m.flatstones | m.standing_stones);
+        if neighbors == 0.into() {
+            p1_eval += 30;
+        }
+    }
+
+    let critical_spaces = placement_threat_map(p1_road_pieces, m.p2_pieces & !m.flatstones);
+    for space in critical_spaces.bits() {
+        if ((space.dilate() ^ space) & p1_road_pieces).count_ones() >= 3 {
+            p1_eval += 50;
+        }
+    }
+
+    let critical_spaces = placement_threat_map(p2_road_pieces, m.p1_pieces & !m.flatstones);
+    for space in critical_spaces.bits() {
+        if ((space.dilate() ^ space) & p2_road_pieces).count_ones() >= 3 {
+            p2_eval += 50;
+        }
+    }
+
+    let mut p1_group_score = match p1_road_pieces.groups().count() {
+        1 => 40,
+        2 => 30,
+        3 => 15,
+        4 => 5,
+        _ => 0,
+    };
+
+    let mut p2_group_score = match p2_road_pieces.groups().count() {
+        1 => 40,
+        2 => 30,
+        3 => 15,
+        4 => 5,
+        _ => 0,
+    };
+
+    if p1_group_score > p2_group_score {
+        p1_group_score *= 2;
+    } else if p2_group_score > p1_group_score {
+        p2_group_score *= 2;
+    } else {
+        match state.to_move() {
+            White => p1_group_score *= 2,
+            Black => p2_group_score *= 2,
+        }
+    }
+
+    let p1_res = state.p1_flatstones + state.p1_capstones;
+    let p2_res = state.p2_flatstones + state.p2_capstones;
+    let mul = ((p1_res + p2_res) as EvalType * 100) / 60;
+
+    p1_eval += p1_group_score * mul / 100;
+    p2_eval += p2_group_score * mul / 100;
+
+    if p1_res < 10 || p2_res < 10 {
+        let mut p1_flats = 2 * m.p1_flat_count as EvalType;
+        let mut p2_flats = 2 * m.p2_flat_count as EvalType + state.komi.as_half_komi() as EvalType;
+
+        if p1_res < p2_res {
+            if state.to_move() == White {
+                p1_flats += 2;
+            }
+        } else if p2_res < p1_res {
+            if state.to_move() == Black {
+                p2_flats += 2;
+            }
+        }
+
+        if p1_flats > p2_flats {
+            p1_eval += (p1_flats - p2_flats) * 100 / p1_res as EvalType;
+        } else if p2_flats > p1_flats {
+            p2_eval += (p2_flats - p1_flats) * 100 / p2_res as EvalType;
+        }
     }
 
     match state.to_move() {
@@ -72,3 +256,25 @@ pub fn evaluate<const N: usize>(state: &State<N>, start_ply: u16) -> Evaluation 
         Black => p2_eval - p1_eval,
     }
 }
+
+struct Weights {
+    pieces: [EvalType; 3],
+    location: [[EvalType; 6]; 6],
+    piece_mul: [(EvalType, EvalType); 3],
+}
+
+const WEIGHT: Weights = Weights {
+    pieces: [100, 40, 80],
+    location: [
+        [00, 05, 05, 05, 05, 00],
+        [05, 10, 15, 15, 10, 05],
+        [05, 15, 20, 20, 15, 05],
+        [05, 15, 20, 20, 15, 05],
+        [05, 10, 15, 15, 10, 05],
+        [00, 05, 05, 05, 05, 00],
+    ],
+    piece_mul: [(-50, 60), (-30, 70), (-20, 90)],
+};
+
+#[cfg(test)]
+mod tests {}
