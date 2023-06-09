f65e4589a1797264de41ed1c1c671752e1e4c9b7
Chris Foster <cdbfoster@gmail.com>
2023-06-09 18:52:16 -0400

    No capstones in the small featureset.

--- analysis/src/evaluation/features/small.rs
+++ analysis/src/evaluation/features/small.rs
@@ -16,7 +16,6 @@ macro_rules! features_small_impl {
                 + 2 * 2                 // Deep friendlies under each piece type
                 + 2 * 2                 // Deep captives under each piece type
                 + 2 * POSITIONS         // Flatstones in each position
-                + 2 * POSITIONS         // Capstones in each position
                 + 2                     // Road groups
                 + 2                     // Lines occupied
                 + 2                     // Unblocked road completion
@@ -30,7 +29,6 @@ macro_rules! features_small_impl {
                 pub reserve_flatstones: f32,
                 pub stack_composition: StackComposition<2>,
                 pub flatstone_positions: [f32; POSITIONS],
-                pub capstone_positions: [f32; POSITIONS],
                 pub road_groups: f32,
                 pub lines_occupied: f32,
                 pub unblocked_road_completion: f32,
@@ -68,11 +66,6 @@ macro_rules! features_small_impl {
                     let p2_flatstones = m.flatstones & m.p2_pieces;
                     let p1_standing_stones = m.standing_stones & m.p1_pieces;
                     let p2_standing_stones = m.standing_stones & m.p2_pieces;
-                    let p1_capstones = m.capstones & m.p1_pieces;
-                    let p2_capstones = m.capstones & m.p2_pieces;
-
-                    let p1_road_pieces = p1_flatstones | p1_capstones;
-                    let p2_road_pieces = p2_flatstones | p2_capstones;
 
                     let p1_flat_count = gather_flat_count(p1_flatstones, 0.0);
                     let p2_flat_count = gather_flat_count(p2_flatstones, self.komi.as_f32());
@@ -88,20 +81,17 @@ macro_rules! features_small_impl {
                     p1.flatstone_positions = gather_positions(p1_flatstones, POSITION_MAPS);
                     p2.flatstone_positions = gather_positions(p2_flatstones, POSITION_MAPS);
 
-                    p1.capstone_positions = gather_positions(p1_capstones, POSITION_MAPS);
-                    p2.capstone_positions = gather_positions(p2_capstones, POSITION_MAPS);
+                    p1.road_groups = (p1_flatstones).groups().count() as f32;
+                    p2.road_groups = (p2_flatstones).groups().count() as f32;
 
-                    p1.road_groups = (p1_road_pieces).groups().count() as f32;
-                    p2.road_groups = (p2_road_pieces).groups().count() as f32;
+                    p1.lines_occupied = gather_lines_occupied(p1_flatstones);
+                    p2.lines_occupied = gather_lines_occupied(p2_flatstones);
 
-                    p1.lines_occupied = gather_lines_occupied(p1_road_pieces);
-                    p2.lines_occupied = gather_lines_occupied(p2_road_pieces);
+                    p1.unblocked_road_completion = gather_road_steps(p1_flatstones, all_pieces & !p1_flatstones);
+                    p2.unblocked_road_completion = gather_road_steps(p2_flatstones, all_pieces & !p2_flatstones);
 
-                    p1.unblocked_road_completion = gather_road_steps(p1_road_pieces, all_pieces & !p1_road_pieces);
-                    p2.unblocked_road_completion = gather_road_steps(p2_road_pieces, all_pieces & !p2_road_pieces);
-
-                    p1.softblocked_road_completion = gather_road_steps(p1_road_pieces, p1_standing_stones | p2_standing_stones | p2_capstones);
-                    p2.softblocked_road_completion = gather_road_steps(p2_road_pieces, p2_standing_stones | p1_standing_stones | p1_capstones);
+                    p1.softblocked_road_completion = gather_road_steps(p1_flatstones, p1_standing_stones | p2_standing_stones);
+                    p2.softblocked_road_completion = gather_road_steps(p2_flatstones, p2_standing_stones | p1_standing_stones);
 
                     p1.standing_stone_blockage = gather_stack_blockage(p1_standing_stones, [p2_flatstones, p2_standing_stones], self);
                     p2.standing_stone_blockage = gather_stack_blockage(p2_standing_stones, [p1_flatstones, p1_standing_stones], self);
